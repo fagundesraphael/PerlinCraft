d2af14c59f7c4ee8b5b33dea066228ebe31d95f0
Raphael Fagundes <fagundesraphael7@gmail.com>
2025-03-23 22:51:44 -0400

    learning to draw

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use draw::{render, Canvas, Color, Drawing, LineBuilder, Style, SvgRenderer};
+
 fn main() {
-    println!("Hello, world!");
+    let mut canvas = Canvas::new(800, 800);
+
+    let line = LineBuilder::new(10.0, 10.0)
+        .curve_to(50.0, 50.0, 20.0, 30.0)
+        .line_to(90.0, 90.0)
+        .build();
+
+    let drawing = Drawing::new()
+        .with_shape(line)
+        .with_style(Style::stroked(2, Color::black()));
+
+    canvas.display_list.add(drawing);
+
+    render::save(&canvas, "img/grap.jpeg", SvgRenderer::new()).expect("Failed to save")
 }
