ef5f044a5ad0ccfd18dd0f1786198fa125faf554
Raphael Fagundes <fagundesraphael7@gmail.com>
2025-03-25 22:31:21 -0400

    add noise and interpolation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
 use draw::{render, Canvas, Color, Drawing, LineBuilder, Style, SvgRenderer};
 use rand::Rng;
 
+fn smoothed_noise(x: i32) -> f64 {
+    (x % 10) as f64 / 10.0
+}
+
+fn linear_interpolation(a: f64, b: f64, t: f64) -> f64 {
+    a + t * (b - a)
+}
+
+fn interpolate_noise(x: f64) -> f64 {
+    let integer_x = x as i32;
+    let fractional_x = x - integer_x as f64;
+
+    let v1 = smoothed_noise(integer_x);
+    let v2 = smoothed_noise(integer_x + 1);
+
+    linear_interpolation(v1, v2, fractional_x)
+}
+
+pub fn noise(x: i32) -> f64 {
+    let x: i32 = (x << 13) ^ x;
+    1.0 - (((x as i64 * (x as i64 * x as i64 * 15731 + 789221) + 1376312589) & 0x7FFFFFFF) as f64
+        / 1073741824.0)
+}
+
 fn main() {
     let mut canvas = Canvas::new(400, 400);
     let mut rand = rand::rng();
